2c6762e6def148b9e7ec9d7bcd433007386a4ef3
JoluiDev <124008612+JoluiDev@users.noreply.github.com>
2025-07-03 18:02:15 -0600

    Mejores y función depositar
    Métodos para:
    volver
    Limpiar datos
    Quitar cuenta
    Depositar

--- PW2_TareaPractica2/src/main/java/uth/hn/pw2_tareapractica2/ATMBean.java
+++ PW2_TareaPractica2/src/main/java/uth/hn/pw2_tareapractica2/ATMBean.java
@@ -14,22 +14,93 @@ import java.util.List;
 public class ATMBean implements Serializable{
     private List<Cuenta> cuentas;
     private Cuenta cuenta;
+    private String numCuenta;
+    private  String pinCuenta;
+    private double deposito;
 
     public ATMBean() {
         this.cuentas = new ArrayList<>();
         this.cuenta = null;
-        cuentas.add(new Cuenta("11111111", "111111", 500));
-        cuentas.add(new Cuenta("11111112", "111112", 1000));
-        cuentas.add(new Cuenta("11111113", "111113", 1500));
-        cuentas.add(new Cuenta("11111114", "111114", 2000));
-        cuentas.add(new Cuenta("11111115", "111115", 2500));
+        this.numCuenta = "";
+        this.pinCuenta = "";
+        this.deposito = 0;
+        cuentas.add(new Cuenta("11111111", "111111","Alejandro Ruíz", 500));
+        cuentas.add(new Cuenta("11111112", "111112","María Núñez", 1000));
+        cuentas.add(new Cuenta("11111113", "111113","Rosa Soto", 1500));
+        cuentas.add(new Cuenta("11111114", "111114","Rodolfo Palma", 2000));
+        cuentas.add(new Cuenta("11111115", "111115","Miguel Flores", 2500));
     }
 
     public String redireccionDeposito (){
+        obtenerCuenta();
         return "depositos.xhtml?faces-redirect=true";
     }
 
     public String redireccionRetiro (){
         return "retiros.xhtml?faces-redirect=true";
+    }
+
+    /*public String mostrarSaldo(){
+        return cuenta.getSaldo() + "";
+    }*/
+    public void obtenerCuenta(){
+        for (Cuenta cuentaa : cuentas) {
+            if (cuentaa.getCuenta().equals(this.numCuenta) && cuentaa.getPin().equals(this.pinCuenta)) {
+                this.cuenta = cuentaa;
+                return;
+            }
+        }
+
+        this.cuenta = null;
+    }
+
+    public String volver(){
+        this.deposito = 0;
+        return "inicio.xhtml?faces-redirect=true";
+    }
+
+    public void cerrarCuenta(){
+        this.numCuenta = "";
+        this.pinCuenta = "";
+        this.cuenta = null;
+    }
+
+    public void depositar (){
+        if(cuenta != null){
+            this.cuenta.setSaldo(this.cuenta.getSaldo() + this.deposito);
+            this.deposito = 0;
+        }
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public String getPinCuenta() {
+        return pinCuenta;
+    }
+
+    public void setPinCuenta(String pinCuenta) {
+        this.pinCuenta = pinCuenta;
+    }
+
+    public double getDeposito() {
+        return deposito;
+    }
+
+    public void setDeposito(double deposito) {
+        this.deposito = deposito;
     }
 }
